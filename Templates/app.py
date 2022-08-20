2268387a98edbaa5a6ffcd18a79a2e3e37ff5a54
alorenz465446 <107652317+alorenz465446@users.noreply.github.com>
2022-08-19 20:39:25 -0400

    py clean up started

--- Templates/app.py
+++ Templates/app.py
@@ -1,1 +1,14 @@
-# flask app
+# flask app (look at lesson 10.5.1)
+
+# import dependecies
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+# set up app
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
